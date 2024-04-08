66ad93b244ac9da27f896d819bffcf53f3c0c6f8
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-04-08 22:10:09 +0300

    Dosyalar 'src' klasörüne taşındı. Veritabanı bilgileri '.env' dosyasından alınıyor.

--- src/config/datastores.js
+++ src/config/datastores.js
@@ -50,9 +50,12 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
-    adapter : 'sails-postgresql',
-    url: 'postgresql://postgres:123456789@localhost:5432/db_seyahatdostu',
+    adapter: 'sails-postgresql',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
   },
-
-
+ 
 };
