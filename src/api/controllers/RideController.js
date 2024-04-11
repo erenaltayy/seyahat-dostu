cf0f02f3b75df4f82f9f0569164c4b1f2943d7ab
Yunus Efe Yılmaz <89478740+yunusefeyilmaz@users.noreply.github.com>
2024-04-11 19:49:18 +0300

    Kullanıcı ve Yolculuk için CRUD işlemleri eklendi. (#5)
    Kullanıcı ve Yolculuk için CRUD işlemleri eklendi.

--- src/api/controllers/RideController.js
+++ src/api/controllers/RideController.js
@@ -6,7 +6,85 @@
  */
 
 module.exports = {
-  
+    create: async function (req, res) {
+        try {
+            const data = req.body;
+            const ride = await Ride.create(data).fetch();
+            return res.ok(ride);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    find: async function (req, res) {
+        try {
+            const rides = await Ride.find();
+            return res.ok(rides);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    findOne: async function (req, res) {
+        try {
+            const id = req.params.id;
+            const ride = await Ride.findOne
+            ({ id });
+            return res.ok(ride);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    update: async function (req, res) {
+        try {
+            const id = req.params.id;
+            const data = req.body;
+            const ride = await Ride.updateOne({ id }).set(data);
+            return res.ok(ride);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    delete: async function (req, res) {
+        try {
+            const id = req.params.id;
+            await Ride.destroyOne({ id });
+            return res.ok();
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    users: async function (req, res) {
+        try {
+            const id = req.params.id;
+            const ride = await Ride.findOne({ id }).populate("users");
+            return res.ok(ride.users);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    join: async function (req, res) {
+        try {
+            const rideId = req.params.rideId;
+            const userId = req.params.userId;
+            const ride = await Ride.findOne({ id: rideId }).populate("users");
+            ride.users.add(userId);
+            await ride.save();
+            return res.ok(ride);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    leave: async function (req, res) {
+        try {
+            const rideId = req.params.rideId;
+            const userId = req.params.userId;
+            const ride = await Ride.findOne({ id: rideId }).populate("users");
+            ride.users.remove(userId);
+            await ride.save();
+            return res.ok(ride);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
 
 };
 
--- src/api/controllers/UserController.js
+++ src/api/controllers/UserController.js
@@ -6,7 +6,83 @@
  */
 
 module.exports = {
-  
+    create: async function (req, res) {
+        try {
+            const data = req.body;
+            const user = await User.create(data).fetch();
+            return res.ok(user);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    find: async function (req, res) {
+        try {
+            const users = await User.find();
+            return res.ok(users);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    findOne: async function (req, res) {
+        try {
+            const id = req.params.id;
+            const user = await User.findOne({ id });
+            return res.ok(user);
+        } catch (error) {
+
+            return res.serverError(error);
+        }
+    },
+    update: async function (req, res) {
+        try {
+            const id = req.params.id;
+            const data = req.body;
+            const user = await User.updateOne({ id }).set(data);
+            return res.ok(user);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    delete: async function (req, res) {
+        try {
+            const id = req.params.id;
+            await User.destroyOne({ id });
+            return res.ok();
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    rides: async function (req, res) {
+        try {
+            const id = req.params.id;
+            const user = await User.findOne({ id }).populate("rides");
+            return res.ok(user.rides);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    joinRide: async function (req, res) {
+        try {
+            const userId = req.params.userId;
+            const rideId = req.params.rideId;
+            const user = await User.findOne({ id: userId }).populate("rides");
+            await User.addToCollection(userId, "rides", rideId);
+            return res.ok(user);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
+    leaveRide: async function (req, res) {
+        try {
+            const userId = req.params.userId;
+            const rideId = req.params.rideId;
+            const user = await User.findOne({ id: userId }).populate("rides");
+            await User.removeFromCollection(userId, "rides", rideId);
+            return res.ok(user);
+        } catch (error) {
+            return res.serverError(error);
+        }
+    },
 
 };
 
