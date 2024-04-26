4dbd8b2ef383d62676a022ba2c28b934620ba7e8
Yunus Efe Yılmaz <89478740+yunusefeyilmaz@users.noreply.github.com>
2024-04-26 20:01:36 +0300

    Veritabanı bağlantı formatı, URL formatına çevrildi. (#8)
    * Veritabanı bağlantı formatı, URL formatına çevrildi.

--- src/config/datastores.js
+++ src/config/datastores.js
@@ -51,11 +51,7 @@ module.exports.datastores = {
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
     adapter: 'sails-postgresql',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    url: 'postgresql://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME,
   },
  
 };
