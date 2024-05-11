96359d19b1e4ef293387a5d9f9d397b64aa2bb1d
enfyna <legendraslan@gmail.com>
2024-05-12 00:44:08 +0300

    Navbar kullanıcı menüsünü tıklanabilir yap.

--- src/assets/js/components/Navbar.jsx
+++ src/assets/js/components/Navbar.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 export default function Navbar() {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+  const toggle = () => {
+    setShowUserMenu(showUserMenu => !showUserMenu);
+  }
+
   return (
     <nav className="bg-gray-800 w-screen">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -40,18 +47,20 @@ export default function Navbar() {
 
             <div className="relative ml-3">
               <div>
-                <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" onClick={toggle} aria-expanded="false" aria-haspopup="true">
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">Kullanıcı menüsünü aç</span>
                   <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                   </img>
                 </button>
               </div>
-              <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">Profil</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Ayarlar</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Çıkış Yap</a>
-              </div>
+              {showUserMenu ?
+                <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">Profil</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Ayarlar</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Çıkış Yap</a>
+                </div>
+                : null}
             </div>
           </div>
         </div>
