7d0100802d8f5424d2aa43c3e3e8b0b917954521
enfyna <legendraslan@gmail.com>
2024-05-14 21:06:52 +0300

    Rezervasyonlarım sayfasını ekle

--- src/assets/js/pages/reservations.jsx
+++ src/assets/js/pages/reservations.jsx
@@ -1,19 +1,32 @@
+import AktifRezervasyonKarti from '@/components/AktifRezervasyonKarti'
+import GecmisRezervasyonKarti from '@/components/GeçmişRezervasyonKarti'
 import { Link } from '@inertiajs/react'
-import Logo from '@/components/Logo'
 
-export default function Index() {
+export default function Reservations() {
   return (
-    <div className="flex f-screen flex-col items-center justify-center space-y-8 text-center text-gray-700">
+    <div className="flex f-screen flex-col space-y-8 text-gray-700 my-12">
 
-      <Link href="/search" className="text-purple-600 underline">
-        Seyahat Bul
-      </Link>
+      <div className="p-4 border-b border-black">
+        <h1 className="font-bold text-black">Rezervasyonlarım</h1>
+      </div>
+      <div className="flex flex-row gap-4 p-4 flex-wrap">
+        <AktifRezervasyonKarti></AktifRezervasyonKarti>
+        <AktifRezervasyonKarti></AktifRezervasyonKarti>
+        <AktifRezervasyonKarti></AktifRezervasyonKarti>
+        <AktifRezervasyonKarti></AktifRezervasyonKarti>
+        <AktifRezervasyonKarti></AktifRezervasyonKarti>
+      </div>
 
-      <div className="flex flex-row items-center justify-center space-x-8">
-        <Logo url="tailwind.com" name="tailwind"></Logo>
-        <Logo url="tailwind.com" name="tailwind"></Logo>
-        <Logo url="tailwind.com" name="tailwind"></Logo>
-        <Logo url="tailwind.com" name="tailwind"></Logo>
+      <div className="p-4 border-b border-black">
+        <h1 className="font-bold text-black">Geçmiş Rezervasyonlarım</h1>
+      </div>
+      <div className="flex flex-row gap-4 p-4 flex-wrap">
+        <GecmisRezervasyonKarti></GecmisRezervasyonKarti>
+        <GecmisRezervasyonKarti></GecmisRezervasyonKarti>
+        <GecmisRezervasyonKarti></GecmisRezervasyonKarti>
+        <GecmisRezervasyonKarti></GecmisRezervasyonKarti>
+        <GecmisRezervasyonKarti></GecmisRezervasyonKarti>
+        <GecmisRezervasyonKarti></GecmisRezervasyonKarti>
       </div>
 
     </div>
